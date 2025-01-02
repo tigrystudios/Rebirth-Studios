9ff06eb7d6a03879a2416fdf3556a480a132bab0
LimonProducts <88504039+tigrystudios@users.noreply.github.com>
2025-01-02 16:09:53 +0000

    yes

--- script.js
+++
@@ -1,8 +0,0 @@
-// Select the hamburger button and sidebar
-const hamburger = document.getElementById('hamburger');
-const sidebar = document.getElementById('sidebar');
-
-// Toggle the sidebar visibility
-hamburger.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-});
